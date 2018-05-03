71dd325c780952f90b95d542214873cf50ac54fb
hiroyuki <hiroyukitamura@gmail.com>
2018-05-03 17:55:26 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,508 @@
+"use strict";
+// const geckodriver = require('geckodriver');
+// const {Builder, By, Key, until} = require('selenium-webdriver');
+// const webdriverio = require('webdriverio');
+
+// (async function example() {
+//     let driver = await new Builder().forBrowser('firefox').build();
+//     driver.manage().logs().getAvailableLogTypes().then((val)=>{
+//         console.log(val);
+//     }).catch((err)=>{
+//         console.log(err);
+//     });
+//     // driver.manage().logs().get(LogType.PERFORMANCE).getAll();
+//     try {
+//         await driver.get('http://radiko.jp/#!/ts/FMJ/20180415150000');
+//         await driver.findElement(By.css('#now-programs-list > div.live-detail__body.group > div.live-detail__text > p.live-detail__play.disabled > a')).click();
+//         await driver.findElement(By.css("#colorbox--term > p:nth-child(4) > a")).click();
+//         await driver.findElement(By.css("#colorbox--term > p.colorbox__btn > a")).click();
+//
+//         await new Promise(resolve => setTimeout(resolve, 10000));
+//     } finally {
+//         await driver.quit();
+//     }
+// })();
+
+// !function () {
+//     const options = {
+//         desiredCapabilities: {
+//             browserName: 'firefox'
+//         }
+//     };
+//
+//     webdriverio
+//         .remote(options)
+//         .init()
+//         .url('http://www.google.com')
+//         .getTitle().then(function(title) {
+//         console.log('Title was: ' + title);
+//     })
+//         .end()
+//         .catch(function(err) {
+//             console.log(err);
+//         });
+// }();
+
+
+
+
+// driver.get('http://radiko.jp/#!/ts/FMJ/20180415150000').then(function(){
+//     console.log('koko');
+//
+//     driver.quit();
+// });
+
+
+// const puppeteer = require('puppeteer');
+
+// const shadowSelectorFn = (el, selector) => el.shadowRoot.querySelector(selector);
+//
+// const queryDeep = async (page, ...selectors) => {
+//     if (!selectors || selectors.length === 0) {
+//         return;
+//     }
+//
+//     const [ firstSelector, ...restSelectors ] = selectors;
+//     let parentElement = await page.$(firstSelector);
+//     let count = 0;
+//     for (const selector of restSelectors) {
+//         parentElement = await page.evaluateHandle(shadowSelectorFn, parentElement, selector);
+//         console.log(count);
+//         count++;
+//     }
+//
+//     return parentElement;
+// };
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+// const isUrl = require('is-url');
+const fs = require('fs-extra');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpeg_static = require('ffmpeg-static');
+const http = require('http');
+const httpProxy = require('http-proxy');
+const request = require('request');
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const rp = require('request-promise');
+const csv = require('csvtojson');
+const exec = require('child_process').exec;
+const iconv = require('iconv-lite');
+// const sudo = require('sudo-prompt');
+const Sudoer = require('electron-sudo').default;
+
+const RADIKO_URL = 'http://radiko.jp/#!/ts/TBS/20180427180000';
+
+function createWindow () {
+    // Create the browser window.
+    console.log('createWindow');
+    let win = new BrowserWindow({width: 800, height: 600});
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    new OpenVpn().init();
+
+    // new GateVpnCsv().requestCsv();
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    console.log('window-all-closed');
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    console.log('active');
+    if (win === null) {
+        createWindow();
+    }
+});
+
+
+class GateVpnCsv {
+    constructor(){
+        this.GATE_VPN_URL = 'http://www.vpngate.net/api/iphone/';
+        this.csvRowArr = [];
+    }
+    requestCsv() {
+        csv()
+            .fromStream(request.get(this.GATE_VPN_URL))
+            .on('csv',(csvRow)=>{
+                if (csvRow[6] === 'JP')
+                    this.csvRowArr.push(csvRow);
+            })
+            .on('done',(error)=>{
+                const log = error ? error : '成功';
+                console.log('requestCsv', log);
+            });
+    }
+}
+
+class OpenVpn {
+    constructor(){
+        const cmd = 'cd C:/Program Files/OpenVPN/bin && openvpn --config "C:/Program Files/OpenVPN/config/vpngate_vpn943899651.opengw.net_tcp_1725.ovpn"';
+        this.cp = exec(cmd);
+        // this.sudoer = new Sudoer({name: 'electron sudo application'});
+    }
+
+    init(){
+        // await this.sudoer.exec(cmd);
+        this.cp.stdout.on('data', function(data) {
+            // const str = iconv.decode(data, "Shift_JIS");
+            console.log('stdout', data);
+        });
+        this.cp.stderr.on('data', function(data) {
+            // const str = iconv.decode(data, "Shift_JIS");
+            console.log('stderr', data);
+        });
+        this.cp.on('close', function(code) {
+            console.log('closing code: ' + code);
+        });
+        this.cp.on('error', function (err) {
+            console.warn('error', err);
+        })
+    }
+}
+
+async function editConfig(page) {
+    // const slcPlayBtnLive = '#now-programs-list > div.live-detail__body.group > div.live-detail__text > p.live-detail__play > a';
+    const slcPlayBtnFree = '#now-programs-list > div.live-detail__body.group > div.live-detail__text > p.live-detail__play.disabled > a';
+    const slcPlayBtn2nd = '#colorbox--term > p.colorbox__btn > a';
+    const chunkListDir = 'chunklist';
+    const ffmpegDir = 'C:\\ffmpeg\\ffmpeg-20180427-4833050-win64-static\\bin\\';
+    ffmpeg.setFfmpegPath(ffmpegDir+'ffmpeg.exe');
+    ffmpeg.setFfprobePath(ffmpegDir+'ffprobe.exe');
+    // https://www.html5rocks.com/ja/tutorials/webcomponents/shadowdom/
+    //     chrome://settings/content/siteDetails?site=http%3A%2F%2Fradiko.jp
+    // await page.goto('chrome://settings/content/flash');
+    // await page.waitFor(3*1000);
+    //
+    // let shadowRoot = await page.evaluate((sel) => {
+    //     return document.getElementsByTagName(sel)[0];
+    // }, 'settings-ui');
+    // console.log('settings-ui', shadowRoot);
+
+
+    // const e = await page.$('#main');
+    // console.log(e.shadowRoot);
+    // const k = await e.$('#main');
+    // console.log(e.shadowRoot);
+    // e.shadowRoot.querySelector('#addSite');
+    // console.log(e);
+    // e.screenshot({path: 'example.png'});
+    // await queryDeep(page, '#settings-ui', '#main');
+    // const main = getShadowRootChild(page.$("#settings-ui"), "#main");
+    // const subPage = getShadowRootChild(main, 'settings-basic-page');
+    // const settingSec = getShadowRootChild(subPage, '#advancedPage > settings-section.expanded');
+    // const f = getShadowRootChild(settingSec, '#advancedPage > settings-section.expanded > settings-privacy-page');
+    //
+    // const result = await page.evaluate(getShadowRootChildText, elHandle, shadowSelector);
+    // await page.screenshot({path: 'example.png'});
+    // await page.type('#input-1', RADIKO_URL);
+    //
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36');
+    await page.goto(RADIKO_URL);
+    page.on('response', response => {
+        console.log(response.status(), response.url());
+        const url = response.url();
+        if (response.url().indexOf('chunklist') !== -1 && path.extname(url) === '.m3u8') {
+            response.text().then(function (status) {
+                if (status.trim() === '') {
+                    console.log('空ファイル', response.url());
+                } else {
+                    if (isFileExists(chunkListDir)) {
+                        fs.removeSync(chunkListDir);
+                    }
+                    const pathE = chunkListDir +'/'+ path.basename(url);
+                    writeFile(pathE, response).then(()=> {
+                        console.log('書き込み完了');
+                        runFfmpeg(pathE);
+                    }).catch(err =>{
+                        console.log(err);
+                    });
+                }
+            });
+        }
+        // const url = response.url().split('?')[0];
+        // if (path.basename(url) === 'playlist.m3u8') {
+        //     response.text().then(function (status) {
+        //         if (status === '')
+        //             return;
+        //         const arr = status.split('\n');
+        //         for (let i = 0; i < arr.length; i++) {
+        //             if (isUrl(arr[i])) {
+        //                 console.log('キタゾー', value);
+        //                 break;
+        //             }
+        //         }
+        //     })
+        // }
+    });
+    // await page.click("#now-programs-list > div.live-detail__body.group > div.live-detail__text > p.live-detail__play > a");
+    // await page.click("#now-programs-list > div.live-detail__body.group > div.live-detail__text > p.live-detail__play.disabled > a");
+    // await page.click("#colorbox--term > p:nth-child(4) > a");
+    await page.waitFor(3*1000);
+    await page.click(slcPlayBtnFree);
+    await page.click(slcPlayBtn2nd);
+    await page.waitFor(3*1000);
+
+    // await page.waitFor(30*60*1000);
+}
+
+async function writeFile(pathE, response) {
+    await fs.outputFile(pathE, await response.buffer());
+}
+
+function isFileExists(filePath) {
+    try {
+        fs.statSync(filePath);
+        return true;
+    } catch(err) {
+        return false;
+    }
+}
+
+function runFfmpeg(pathE) {
+    ffmpeg(pathE)
+        .setFfmpegPath(ffmpeg_static.path)
+        .audioCodec('copy')
+        .videoCodec('copy')
+        .on('start', function(commandLine) {
+            console.log('Spawned Ffmpeg with command: ' + commandLine);
+        })
+        .on('error', function(err, stdout, stderr) {
+            console.log('Cannot process video: ' + err.message);
+        })
+        .on('end', function(stdout, stderr) {
+            console.log('Transcoding s  ucceeded !');
+        }).on('progress', function(progress) {
+            console.log('Processing: ' + progress.percent + '% done');
+        })
+        .inputOptions([
+            '-protocol_whitelist', 'file,http,https,tcp,tls,crypto'
+        ])
+        .outputOptions([
+            '-bsf:a aac_adtstoasc'
+        ])
+        .output('output.mp4')
+        .run();
+}
+
+(async () => {
+    // const FLASH_PATH = 'C:\\windows\\system32\\Macromed\\Flash\\pepflashplayer64_29_0_0_140.dll';
+    // const browser = await puppeteer.launch({
+    //     headless: false,
+    //     // userDataDir: 'User Data',
+    //     executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    //     args: [
+    //         '--enable-usermedia-screen-capturing',
+    //         '--allow-http-screen-capture',
+    //         '--no-sandbox',
+    //         // '--no-sandbox',
+    //         // '--ppapi-flash-path= '+ FLASH_PATH,
+    //         // "--allow-running-insecure-content",
+    //         // "--allow-insecure-websocket-from-https-origin",
+    //         // "--allow-outdated-plugins",
+    //     ]
+    // });
+    // const page = await browser.newPage();
+    //
+    // // await connect(page);
+    //
+    // await browser.close();
+
+    // customAccess();
+
+    // testSample();
+})();
+
+async function testSample() {
+    const browser = await puppeteer.launch({
+        // Ug: https://bugs.chromium.org/p/chromium/issues/detail?id=706008
+        headless: false,
+        // TODO: smarter way to find this
+        // Ug: https://bugs.chromium.org/p/chromium/issues/detail?id=769894
+        executablePath: 'C:\\Program Files (x86)\\Google\\Chrome Dev\\Application\\chrome.exe',
+        args: [
+            // '--auto-open-devtools-for-tabs',
+            '--auto-select-desktop-capture-source=pickme',
+            '--disable-infobars',
+            '--load-extension=' + __dirname,  // eslint-disable-line no-path-concat
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // No autoplay
+            '--autoplay-policy=user-gesture-required'
+        ],
+        slowMo: 100
+    });
+    const page = await browser.newPage();
+}
+
+async function connect(page) {
+    page.on('response', response => {
+        // console.log(response.status(), response.url());
+    });
+    await page.goto('https://abema.tv/now-on-air/special-plus-2');
+    await page.waitFor(20*1000);
+
+    await page.evaluate(()=>{
+        const session = {
+            audio: false,
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'screen',
+                },
+                optional: []
+            },
+        };
+        navigator.webkitGetUserMedia(session, (stream)=>{
+            let chunks=[], recorder = new MediaRecorder(stream, {
+                videoBitsPerSecond: 250000,
+                ignoreMutedMedia: true,
+                mimeType: 'video/webm'
+            });
+            recorder.ondataavailable = function (event) {
+                console.log('recorder.ondataavailable');
+                if (event.data.size > 0) {
+                    chunks.push(event.data);
+                }
+            };
+
+            recorder.onstop = function () {
+                console.log('録画ストップ');
+                let superBuffer = new Blob(chunks, {
+                    type: 'video/webm'
+                });
+
+                const url = URL.createObjectURL(superBuffer);
+                const a = document.createElement('a');
+                document.body.appendChild(a);
+                a.style = 'display: none';
+                a.href = url;
+                a.download = 'test.webm';
+                a.click();
+            };
+
+            console.log('録画スタート');
+            recorder.start();
+
+            setTimeout(()=>{
+                recorder.stop()
+            }, 5000);
+        }, (args)=>{
+            console.log(args)
+        });
+    });
+
+    await page.waitFor(20*1000);
+}
+
+function customAccess() {
+    //
+    // http.createServer(function(req, res) {
+    //     // You can define here your custom logic to handle the request
+    //     // and then proxy the request.
+    //     proxy.web(req, res, {
+    //         target: 'http://127.0.0.1:5060'
+    //     }, function (err) {
+    //         console.log(err);
+    //     });
+    // }).listen(8000);
+
+    // const proxy = new httpProxy.createProxyServer({
+    //     target: 'http://kakunin.net'
+    // });
+    // proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    //     proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+    // }).on('proxyRes', function (proxyRes, req, res) {
+    //     console.log(res.toString());
+    // }).on('error', function (err, req, res) {
+    //     res.writeHead(500, {
+    //         'Content-Type': 'text/plain'
+    //     });
+    //
+    //     res.end('Something went wrong. And we are reporting a custom error message.');
+    // });
+    //
+    // const proxyServer = http.createServer(function (req, res) {
+    //     proxy.web(req, res, {
+    //         target: 'http://kakunin.net'
+    //     }, function (err) {
+    //         console.log('err', err.toString());
+    //     });
+    // });
+    // proxyServer.listen(8080);
+    // const proxy = httpProxy.createServer({
+    //     target:'122.183.137.190:8080'
+    // });
+    // proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    //         console.log('proxyReq', res.body);
+    //         proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+    //     }).on('proxyRes', function (proxyRes, req, res) {
+    //         console.log('proxyRes');
+    //     }).on('error', function (err, req, res) {
+    //         res.writeHead(500, {
+    //             'Content-Type': 'text/plain'
+    //         });
+    //     res.end('Something went wrong. And we are reporting a custom error message.');
+    // });
+    // proxy.listen(8080);
+    //
+    // httpProxy.createServer().listen(8080);
+
+    const proxy = request.defaults({'proxy': 'http://125.175.176.29:8080'});
+    const server = http.createServer(function (req, res) {
+        proxy.get('http://www.google.co.jp/');
+    });
+    server.listen(8080);
+    // const proxy = request.defaults({'proxy': 'http://122.183.137.190:8080'});
+    // proxy.get("http://github.com/", function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         console.log(body);
+    //     } else {
+    //         console.log(error);
+    //     }
+    // });
+
+    // httpProxy.createProxyServer({
+    //     target: 'http://www.ugtop.com/spill.shtml'
+    // }).listen(8080);
+
+    // const https = require('https');
+    // const HttpsProxyAgent = require('https-proxy-agent');
+    // const proxyE = 'http://122.183.137.8080';
+    // const agent = new HttpsProxyAgent(proxyE);
+    // const post_req = https.request({
+    //     uri: "https://www.npmjs.com/package/https-proxy-agent",
+    //     host: 'localhost',
+    //     port: '443',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     agent: agent,
+    //     timeout: 10000,
+    //     followRedirect: true,
+    //     maxRedirects: 10
+    // }, function(res) {
+    //     res.setEncoding('utf8');
+    //     res.on('data', function(chunk) {
+    //         console.log('Response: ' + chunk);
+    //     });
+    // });
+    // post_req.write("name=john");
+    // post_req.end();
+}
---
+++ main.js
@@ -0,0 +1,133 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+// Gather the params
+const args = {};
+for (const arg of process.argv) {
+  if (arg.startsWith('--')) {
+    const equalsIndex = arg.indexOf('=');
+    if (equalsIndex !== -1)
+      args[arg.substr(2, equalsIndex - 2)] = arg.substr(equalsIndex + 1);
+  }
+}
+if (!args.url) throw new Error('URL required');
+
+(async () => {
+  // Fire up the browser
+  const browser = await puppeteer.launch({
+    // Ug: https://bugs.chromium.org/p/chromium/issues/detail?id=706008
+    headless: false,
+    // TODO: smarter way to find this
+    // Ug: https://bugs.chromium.org/p/chromium/issues/detail?id=769894
+    executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+    args: [
+      // '--auto-open-devtools-for-tabs',
+      '--auto-select-desktop-capture-source=pickme',
+      '--disable-infobars',
+      '--load-extension=' + __dirname,  // eslint-disable-line no-path-concat
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // No autoplay
+      '--autoplay-policy=user-gesture-required'
+    ],
+    slowMo: 100
+  });
+
+  const page = await browser.newPage()
+
+  let outStream = null
+  await page.exposeFunction('recorderStart', () => {
+    if (!outStream) {
+      const fileName = args.file || 'out.webm'
+      console.log('Opening ' + fileName)
+      outStream = fs.createWriteStream(fileName, 'binary')
+    }
+  })
+  await page.exposeFunction('recorderBlob', blob => {
+    // Comes in here as a string
+    if (outStream) {
+      console.log('Writing ' + blob.length + ' bytes to ' + outStream.path)
+      outStream.write(blob, 'binary')
+    }
+  })
+  let recorderStopped = false
+  await page.exposeFunction('recorderStop', () => {
+    console.log('Rec end', recorderStopped)
+    if (outStream && !recorderStopped) {
+      console.log('Closing ' + outStream.path)
+      outStream.end()
+      // Set a window variable saying we're all done
+      recorderStopped = true
+      page.evaluate(() => window.recorderStopped = true)
+    }
+  })
+  await page.evaluateOnNewDocument(() => {
+    window.addEventListener('message', event => {
+      // Only handle backend messages here
+      if (!event.data.type || !event.data.type.startsWith('REC_BACKEND_')) return
+      switch (event.data.type) {
+        case 'REC_BACKEND_START':
+          window.recorderStart()
+          break
+        case 'REC_BACKEND_STOP':
+          window.recorderStop()
+          break
+        case 'REC_BACKEND_BLOB':
+          window.recorderBlob(event.data.blob)
+          break
+        default:
+          console.log('Unrecognized message', event.data)
+      }
+    })
+  })
+  await page.goto(args.url)
+
+  // Go over all "click-" and do the clicking as necessary
+  for (const argName in args) {
+    if (argName.startsWith('click-') && !argName.startsWith('click-delay-')) {
+      const selector = args[argName]
+      const delay = args['click-delay-' + argName.substr(6)]
+      const doWait = () => {
+        console.log('Waiting for', selector, 'delay', delay)
+        page.waitForSelector(selector)
+          .then(() => {
+            console.log('Found selector', selector, 'clicking after delay')
+            return page.waitFor(delay ? parseInt(delay, 10) : 0)
+          })
+          .then(() => page.click(selector).catch(() => doWait()))
+      }
+      doWait()
+    }
+  }
+
+  // Wait to see if recorder stopped or N seconds. Returns false on timeout, true if stopped
+  waitForStopOrTimeout = async (seconds) =>
+    await page.waitForFunction('window.recorderStopped', { timeout: seconds * 1000 })
+      .then(() => true)
+      .catch(error =>
+        error.message && error.message.indexOf('timeout') !== -1 ? Promise.resolve(false) : Promise.reject(error)
+      )
+
+  // Loop while waiting for it to stop
+    let i = 0;
+  while (true) {
+    console.log('Waiting for stop or timeout')
+    const stopped = await waitForStopOrTimeout(30)
+    if (i<3) {
+      console.log('Timed out, stopping all video')
+      await page.evaluate(() => {
+        for (const videoElem of document.querySelectorAll('video')) {
+          videoElem.pause()
+        }
+        window.postMessage({ type: 'REC_CLIENT_STOP' }, '*')
+      })
+    } else {
+      console.log('Stopped')
+      break
+    }
+    i++
+  }
+
+  console.log('Closing browser')
+  await browser.close()
+})()
