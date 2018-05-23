2fd388bb91cf0a9730255877ef81c59382d582d7
hiroyuki <hiroyukitamura@gmail.com>
2018-05-24 01:58:30 +0900

    うぇーい・・・

--- index.js
+++ index.js
@@ -201,6 +201,7 @@ class PuppeteerOperator {
     }
 }
 
+
 String.prototype.splice = function(start, delCount, newSubStr){
     return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
 };
